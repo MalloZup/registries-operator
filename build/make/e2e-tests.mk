b08889ac4e9b6496134643db42b7f0f2043a7b07
dmaiocchi <dmaiocchi@suse.com>
2019-01-07 23:35:53 +0100

    fx

--- build/make/e2e-tests.mk
+++ build/make/e2e-tests.mk
@@ -2,10 +2,10 @@
 # End To End Tests k8s operator
 #############################################################
 
-KUBECONFIG := $(shell kind get kubeconfig-path || echo $KUBECONFIG)
+KIND_KUBECONFIG := $(shell kind get kubeconfig-path)
 e2e-tests:
-	kubectl get pods  --kubeconfig="$(KUBECONFIG)"
+	kubectl get pods --kubeconfig="$(KIND_KUBECONFIG)"
 	echo " -- cluster info --"
-	kubectl cluster-info -v5 --kubeconfig="$(KUBECONFIG)"
+	kubectl cluster-info -v5 --kubeconfig="$(KIND_KUBECONFIG)"
 	echo "deploying operator"
-	kubectl apply -f deployments/registries-operator-full.yaml --kubeconfig="$(KUBECONFIG)"
+	kubectl apply -f deployments/registries-operator-full.yaml --kubeconfig="$(KIND_KUBECONFIG)"
