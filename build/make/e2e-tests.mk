feb47e221da5bafc322b520505e13c0e299a2b94
dmaiocchi <dmaiocchi@suse.com>
2019-01-07 18:10:19 +0100

    fixup

--- build/make/e2e-tests.mk
+++ build/make/e2e-tests.mk
@@ -3,5 +3,10 @@
 #############################################################
 
 e2e-tests:
+	cp `kind get kubeconfig-path --name="1"` ~/.kube/config
+	echo "-- cluster info --"
+	kubectl cluster-info -v5
+	kubectl config view
+	echo
 	echo "deploying operator"
 	kubectl apply -f deployments/registries-operator-full.yaml
