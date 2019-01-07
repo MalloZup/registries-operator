2d81709d901abf4756dd388ef48eb984d5ac35cb
dmaiocchi <dmaiocchi@suse.com>
2019-01-07 23:26:08 +0100

    fixup

--- build/make/e2e-tests.mk
+++ build/make/e2e-tests.mk
@@ -6,10 +6,8 @@ KUBECONFIG := $(shell kind get kubeconfig-path || echo "unknown")
 e2e-tests:
 	export KUBECONFIG=$(KUBECONFIG)
 	echo "$$KUBECONFIG"
-	kubectl get pods --kubeconfig="$(KUBECONFIG)"
+	kubectl get pods  --kubeconfig="$(KUBECONFIG)"
 	echo " -- cluster info --"
-	kubectl cluster-info -v5
-	kubectl config view
-	echo
+	kubectl cluster-info -v5 --kubeconfig="$(KUBECONFIG)"
 	echo "deploying operator"
-	kubectl apply -f deployments/registries-operator-full.yaml
+	kubectl apply -f deployments/registries-operator-full.yaml --kubeconfig="$(KUBECONFIG)"
